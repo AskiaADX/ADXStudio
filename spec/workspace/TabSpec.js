3ee557c47045f5ad7da594753031efdf26b4e183
Mamadou Sy <mamadou@askia.com>
2015-07-03 20:04:32 +0200

    Detect the file is text or binary and load the content accordingly

--- spec/workspace/TabSpec.js
+++ spec/workspace/TabSpec.js
@@ -1,6 +1,7 @@
 describe("Tab", function () {
    var Tab,
-       uuid = require('node-uuid');
+       uuid = require('node-uuid'),
+       fs   = require('fs');
 
     beforeEach(function () {
        Tab = require('../../src/workspace/Tab.js').Tab;
@@ -24,7 +25,71 @@ describe("Tab", function () {
             expect(tab.type).toBe('file');
             expect(tab.path).toBe('path/of/file1');
         });
-
     });
 
+    describe("#loadFile", function () {
+        it("should set the #fileType to `text` if the file is a text file", function () {
+            spyOn(Tab, 'isTextOrBinaryFile').andCallFake(function (path, cb) {
+                cb(null, 'text');
+            });
+
+            var tab = new Tab({
+                path : 'file/to/load'
+            });
+
+            tab.loadFile(function () {
+                expect(tab.fileType).toBe('text');
+            });
+        });
+
+
+        it("should set the #fileType to `binary` if the file is a text file", function () {
+            spyOn(Tab, 'isTextOrBinaryFile').andCallFake(function (path, cb) {
+                cb(null, 'binary');
+            });
+
+            var tab = new Tab({
+                path : 'file/to/load'
+            });
+
+            tab.loadFile(function () {
+                expect(tab.fileType).toBe('binary');
+            });
+        });
+
+        it("should, if the file is `text`, read the content and assign it to the #content property", function () {
+            spyOn(Tab, 'isTextOrBinaryFile').andCallFake(function (path, cb) {
+                cb(null, 'text');
+            });
+            spyOn(fs, 'readFile').andCallFake(function (path, cb) {
+                cb(null, 'Hello');
+            });
+
+            var tab = new Tab({
+                path : 'file/to/load'
+            });
+
+            tab.loadFile(function () {
+                expect(tab.content).toBe('Hello');
+            });
+        });
+
+        it("should not read the file, if the file is `binary`", function () {
+            spyOn(Tab, 'isTextOrBinaryFile').andCallFake(function (path, cb) {
+                cb(null, 'binary');
+            });
+            spyOn(fs, 'readFile').andCallFake(function (path, cb) {
+                cb(null, 'Should not be set');
+            });
+
+            var tab = new Tab({
+                path : 'file/to/load'
+            });
+
+            tab.loadFile(function () {
+                expect(tab.content).toBe(null);
+            });
+        });
+
+    });
 });
--- src/workspace/Tab.js
+++ src/workspace/Tab.js
@@ -1,5 +1,6 @@
 var uuid = require('node-uuid');
-
+var detector = require('charset-detector');
+var fs = require('fs');
 
 /**
  * Tab in pane
@@ -16,6 +17,55 @@ function Tab(config) {
     this.name = this.config.name || '';
     this.type = this.config.type || '';
     this.path = this.config.path || '';
+    this.fileType = '';
+    this.content  = null;
 }
+
+/**
+ * Load the file and initialize additional properties in the tab object
+ *
+ * @param callback
+ */
+Tab.prototype.loadFile = function loadFile(callback) {
+    var self = this;
+    Tab.isTextOrBinaryFile(this.path, function (err, type) {
+        self.fileType = type;
+        if (type === 'text') {
+            fs.readFile(self.path, function (err, data) {
+                self.content = data.toString();
+                callback();
+            });
+        } else {
+            callback();
+        }
+    });
+};
+
+/**
+ * Test if the file at the specified path is text or binary
+ * @param {String} path Path of the file to test
+ * @param {Function} callback Callback function
+ * @param {Error} callback.err Error
+ * @param {String} callback.err Error
+ */
+Tab.isTextOrBinaryFile = function (path, callback) {
+
+    fs.open(path, 'r', function(err, fd) {
+        if (err) {
+            callback(err, "");
+            return;
+        }
+        var bufferSize  = 16000,
+            buffer      = new Buffer(bufferSize);
+
+        fs.read(fd, buffer, 0, bufferSize, 0);
+        fs.close(fd);
+
+        var charsets = detector(buffer);
+        var fileType = (charsets[0].confidence < 10) ? 'binary' : 'text';
+        callback(null, fileType);
+    });
+
+};
 
 exports.Tab = Tab;
