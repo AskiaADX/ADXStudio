f7b755f547c4e00671e2c0349198a37020959a13
Mamadou Sy <mamadou@askia.com>
2015-12-18 18:55:20 +0100

    Prepare the version 0.0.5

--- pack.js
+++ pack.js
@@ -2,13 +2,13 @@ var path = require("path");
 var fs = require('fs');
 var packager = require('electron-packager');
 
-var outputPath = path.join(process.cwd(), "releases/0.0.0.4/");
+var outputPath = path.join(process.cwd(), "releases/0.0.0.5/");
 var opts = {
     "dir"  : process.cwd(),
     "name" : "ADXStudio",
     "platform" : "win32",
     "arch" : "all",
-    "version" : "0.36.0",
+    "version" : "0.36.1",
     "out" : outputPath,
     "icon" : path.join(process.cwd(), "app/adx-studio_icon.ico"),
     "overwrite" : true,
@@ -18,8 +18,8 @@ var opts = {
         "LegalCopyright" : "Copyright (c) Askia 2016",
         "FileDescription" : "Askia Design eXtension - Studio",
         "OriginalFilename" : "ADXStudio",
-        "FileVersion" : "0.0.0.4",
-        "ProductVersion" : "0.0.0.4",
+        "FileVersion" : "0.0.0.5",
+        "ProductVersion" : "0.0.0.5",
         "ProductName" : "ADXStudio",
         "InternalName" : "ADXStudio"
     }
